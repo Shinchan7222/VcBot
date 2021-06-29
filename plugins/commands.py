d757afc21b46c0c8f3f886bd8ca11d62e3c9b22d
Shinchan7222 <83214906+Shinchan7222@users.noreply.github.com>
2021-06-29 15:52:35 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -2,33 +2,18 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram import Client, filters
 
 
-REPO = "**🗂️ Repo :** [VcBot Repo](https://github.com/The-HellBot/VcBot)\n\n🌟 **Github :** [The-HellBot](https://github.com/The-HellBot) \n\n**📍   [Group](https://t.me/hellbot_chat)  &  [Channel](https://t.me/its_hellbot)   📍**"
-HOME_TEXT = "💖 **Hi [{}](tg://user?id={})**,\n\nI'm **Hêll Music Bot** \nI Can Play Radio/Stream Music In Channels & Groups 24x7 Nonstop!\n\n**😉 Happy Streaming 😉**"
-HELP = """**Join @The_HellBot and @Its_Fuckin_Hell to get more help!!
+REPO = "**jaa na lode**"
+HOME_TEXT = "💖 **Hi [{}](tg://user?id={})**,\n\njaa na lode**"
+HELP = """**Join @The_HellBot and @Its_Fuckin_Hell to get more dick!!
 
 🏷️ **Users Commands**:
-\u2022 `/play`  -  Reply to an audio to play or add to queue.
-\u2022 `/help`  -  Shows help for commands.
-\u2022 `/playlist`  -  Shows the playlist.
-\u2022 `/current`  -  Shows playing time of current track.
-\u2022 `/song song name`  -  Download the song.
-
-🏷️ **Admin Commands**:
-\u2022 `/skip x`  -  Skip current or x song. [ x >= 2 ]
-\u2022 `/join`  -  Join voice chat of current group.
-\u2022 `/leave`  -  Leave current voice chat.
-\u2022 `/vc`  -  Check which VC is joined.
-\u2022 `/stop`  -  Stop playing music.
-\u2022 `/radio`  -  Start radio stream.
-\u2022 `/stopradio`  -  Stop radio stream.
-\u2022 `/replay`  -  Play from the beginning.
-\u2022 `/clean`  -  Remove unused RAW PCM files.
+\u2022 PCM files.
 \u2022 `/pause`  -  Pause playing music.
 \u2022 `/resume`  -  Resume playing music.
 \u2022 `/mute`  -  Mute the VC Bot.
 \u2022 `/unmute`  -  Unmute the VC Bot.
-\u2022 `/restart`  -  Restart the bot.
-"""
+
+
 
 
 @Client.on_message(filters.command('start'))
@@ -40,7 +25,7 @@ async def start(client, message):
     ],
     [
         InlineKeyboardButton('📑 GitHub', url='https://github.com/The-HellBot'),
-        InlineKeyboardButton('📜 VcBot Repo', url='https://github.com/The-HellBot/VcBot'),
+
     ],
     [
         InlineKeyboardButton('⚙️ HELP ⚙️', callback_data='help'),
